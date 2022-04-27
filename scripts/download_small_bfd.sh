235a80ce6ff08bff04219a78a74871ec8ccc1707
Christian Meesters <meesters@uni-mainz.de>
2022-04-27 07:56:00 +0200

    scripts/download_small_bfd.sh - added overwrite protection and fixed error when pushing to  and trying to gunzip /

--- scripts/download_small_bfd.sh
+++ scripts/download_small_bfd.sh
@@ -34,8 +34,21 @@ ROOT_DIR="${DOWNLOAD_DIR}/small_bfd"
 SOURCE_URL="https://storage.googleapis.com/alphafold-databases/reduced_dbs/bfd-first_non_consensus_sequences.fasta.gz"
 BASENAME=$(basename "${SOURCE_URL}")
 
+if [ -d "${ROOT_DIR}" ]; then
+    echo "WARNING: Destination directory '${ROOT_DIR}' does already exist."
+    read -p "Proceed by deleting existing download directory? [Y/n]" -n1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Nn]$ ]]; then
+        echo "INFO: Deleting previous download directory: '${ROOT_DIR}'"
+        rm -rf "${ROOT_DIR}"
+    else
+        echo "Aborting download."
+        exit 0
+    fi
+fi
+
 mkdir --parents "${ROOT_DIR}"
 aria2c "${SOURCE_URL}" --dir="${ROOT_DIR}"
 pushd "${ROOT_DIR}"
-gunzip "${ROOT_DIR}/${BASENAME}"
+gunzip "${BASENAME}"
 popd
