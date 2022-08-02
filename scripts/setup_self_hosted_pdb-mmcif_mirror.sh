30801b2692bc40194e239979a2b22aeebc2d42ac
yaoyy.hi@gmail.com <33014714+YaoYinYing@users.noreply.github.com>
2022-08-02 16:47:18 +0800

    Update setup_self_hosted_pdb-mmcif_mirror.sh

--- scripts/setup_self_hosted_pdb-mmcif_mirror.sh
+++ scripts/setup_self_hosted_pdb-mmcif_mirror.sh
@@ -95,7 +95,7 @@ echo \"-------------------------------------------------------------------------
 
 echo "${CRONTAB_CMD}" > "${db_path}/pdb_mmcif_rsync.sh"
 touch /var/spool/cron/$(whoami)
-echo "30 16 * * 5 ${db_path}/pdb_mmcif_rsync.sh" >> /var/spool/cron/$(whoami)
+echo "30 16 * * 5 ${db_path}/pdb_mmcif_rsync.sh >> ${db_path}/pdb_mmcif_rsync.log 2>&1" >> /var/spool/cron/$(whoami)
 
 
 
