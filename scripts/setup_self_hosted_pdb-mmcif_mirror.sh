e3eec2893669e40d76949900f6b563c0408f1db9
yaoyy.hi@gmail.com <33014714+YaoYinYing@users.noreply.github.com>
2022-08-02 18:04:35 +0800

    Update setup_self_hosted_pdb-mmcif_mirror.sh

--- scripts/setup_self_hosted_pdb-mmcif_mirror.sh
+++ scripts/setup_self_hosted_pdb-mmcif_mirror.sh
@@ -3,13 +3,22 @@
 set -e
 db_path=$(readlink -f $1)
 
+echo "----------------------------------------------------------------------------"
+echo "Checking for linux distribution ..."
+if [[ "$(which yum)" != "" ]];then
+  LINUX_DIST="REDHAT"
+elif [[ "$(which apt)" != "" ]];then
+  LINUX_DIST="DEBIAN"
+else
+  LINUX_DIST="DEBIAN"
+fi
+echo "----------------------------------------------------------------------------"
+echo "Checking for rsync installation ..."
 if [[ "$(which rsync)" == "" ]];then
   echo "Rsync not available."
-  if [[ "$(which yum)" != "" ]];then
-    LINUX_DIST="REDHAT"
+  if [[ "${LINUX_DIST}" == "REDHAT" ]];then
     sudo yum install rsync -y
-  elif [[ "$(which apt)" != "" ]];then
-    LINUX_DIST="DEBIAN"
+  elif [[ "${LINUX_DIST}" == "DEBIAN" ]];then
     sudo apt install rsync -y
   else
     echo "Error: Please install rsync by yourself."
@@ -47,6 +56,12 @@ list=no
 
 hosts allow=* "
 
+echo "----------------------------------------------------------------------------"
+echo "Now configure rsyncd ..."
+echo "Your configurations are listed below:"
+echo "----------------------------------------------------------------------------"
+echo ${rsync_configuration}
+echo "----------------------------------------------------------------------------"
 sudo systemctl start rsyncd.service
 sudo systemctl enable rsyncd.service
 echo ${rsync_configuration} >/etc/rsyncd.conf
@@ -56,14 +71,15 @@ sudo systemctl restart rsyncd.service
 echo "----------------------------------------------------------------------------"
 echo "Modifying firewall configuration in expected linux distribution ${LINUX_DIST} ... "
 if [[ "$LINUX_DIST" == "CENTOS" ]]; then
-  sudo firewall-cmd --zone=public --add-port=873/tcp --permanent || exit 1
+  cmd="sudo firewall-cmd --zone=public --add-port=873/tcp --permanent"
+  echo "$cmd" && eval "$cmd" || exit 1
 elif [[ "$LINUX_DIST" == "DEBIAN" ]]; then
-  sudo ufw allow 873 && sudo ufw reload || exit 1
+  cmd="sudo ufw allow 873 && sudo ufw reload"
+  echo "$cmd" && eval "$cmd" || exit 1
 fi
 
 echo "----------------------------------------------------------------------------"
 echo "Now we start to synchronize from original mirrors ..."
-
 mkdir --parents "${db_path}/pdb_mmcif"
 
 echo "Trying to sync with PDBj ..." && \
@@ -75,12 +91,12 @@ MIRROR_CMD="rsync --recursive --links --perms --times --compress --info=progress
 echo "${MIRROR_CMD}" && eval "${MIRROR_CMD}" || \
 echo "Trying to sync with RCSB PDB ..." && MIRROR="RCSB" && \
 MIRROR_CMD="rsync --recursive --links --perms --times --compress --info=progress2 --delete --port=33444 rsync.rcsb.org::ftp_data/structures/divided/mmCIF/  ${db_path}/pdb_mmcif/raw/" &&
-echo "${MIRROR_CMD}" && eval "${MIRROR_CMD}" || echo Failed to run all rsync tests && exit 1
+echo "${MIRROR_CMD}" && eval "${MIRROR_CMD}" || echo "Failed to run all rsync tests" && exit 1
+
 echo "----------------------------------------------------------------------------"
 echo "Done! Mirror site: ${MIRROR}"
 echo "The following command will be executed every Friday:"
 echo "${MIRROR_CMD}"
-
 echo "----------------------------------------------------------------------------"
 echo "Now we create a crontab task."
 CRONTAB_CMD="#!/bin/bash
@@ -92,10 +108,15 @@ endDate=\`date +\"%Y-%m-%d %H:%M:%S\"\`
 echo \"★[\${endDate}] Successful\"
 echo \"----------------------------------------------------------------------------\"
 "
-
+echo "Your crontab task configuration is listed here:"
+echo "----------------------------------------------------------------------------"
+echo "${CRONTAB_CMD}"
+echo "----------------------------------------------------------------------------"
 echo "${CRONTAB_CMD}" > "${db_path}/pdb_mmcif_rsync.sh"
 touch /var/spool/cron/$(whoami)
 echo "30 16 * * 5 ${db_path}/pdb_mmcif_rsync.sh >> ${db_path}/pdb_mmcif_rsync.log 2>&1" >> /var/spool/cron/$(whoami)
+
+echo "Congrats! Now you have a mirror site against the ${MIRROR}!"
 
 
 
