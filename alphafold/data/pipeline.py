e34af6e16d1b81ef4774b4965a579eaff1bbcab5
Umberto Lupo <umberto.lupo@gmail.com>
2023-03-01 12:03:29 +0100

    Short-cut template searching if max_template_date is < 1 Jan 1971

--- alphafold/data/pipeline.py
+++ alphafold/data/pipeline.py
@@ -14,6 +14,7 @@
 
 """Functions for building the input features for the AlphaFold model."""
 
+import datetime
 import os
 from typing import Any, Mapping, MutableMapping, Optional, Sequence, Union
 from absl import logging
@@ -31,6 +32,9 @@ import numpy as np
 
 FeatureDict = MutableMapping[str, np.ndarray]
 TemplateSearcher = Union[hhsearch.HHSearch, hmmsearch.Hmmsearch]
+
+# No PDB structures exist before this date
+PDB_START_DATE = datetime.datetime(1971, 1, 1)
 
 
 def make_sequence_features(
@@ -183,25 +187,30 @@ class DataPipeline:
     msa_for_templates = parsers.remove_empty_columns_from_stockholm_msa(
         msa_for_templates)
 
-    if self.template_searcher.input_format == 'sto':
-      pdb_templates_result = self.template_searcher.query(msa_for_templates)
-    elif self.template_searcher.input_format == 'a3m':
-      uniref90_msa_as_a3m = parsers.convert_stockholm_to_a3m(msa_for_templates)
-      pdb_templates_result = self.template_searcher.query(uniref90_msa_as_a3m)
+    _max_template_date = self.template_featurizer._max_template_date
+    # If max_template_date is before start of the PDB, skip this step
+    if _max_template_date < PDB_START_DATE:
+      pdb_template_hits = []
     else:
-      raise ValueError('Unrecognized template input format: '
-                       f'{self.template_searcher.input_format}')
+      if self.template_searcher.input_format == 'sto':
+        pdb_templates_result = self.template_searcher.query(msa_for_templates)
+      elif self.template_searcher.input_format == 'a3m':
+        uniref90_msa_as_a3m = parsers.convert_stockholm_to_a3m(msa_for_templates)
+        pdb_templates_result = self.template_searcher.query(uniref90_msa_as_a3m)
+      else:
+        raise ValueError('Unrecognized template input format: '
+                         f'{self.template_searcher.input_format}')
 
-    pdb_hits_out_path = os.path.join(
-        msa_output_dir, f'pdb_hits.{self.template_searcher.output_format}')
-    with open(pdb_hits_out_path, 'w') as f:
-      f.write(pdb_templates_result)
+      pdb_hits_out_path = os.path.join(
+          msa_output_dir, f'pdb_hits.{self.template_searcher.output_format}')
+      with open(pdb_hits_out_path, 'w') as f:
+        f.write(pdb_templates_result)
+
+      pdb_template_hits = self.template_searcher.get_template_hits(
+          output_string=pdb_templates_result, input_sequence=input_sequence)
 
     uniref90_msa = parsers.parse_stockholm(jackhmmer_uniref90_result['sto'])
     mgnify_msa = parsers.parse_stockholm(jackhmmer_mgnify_result['sto'])
-
-    pdb_template_hits = self.template_searcher.get_template_hits(
-        output_string=pdb_templates_result, input_sequence=input_sequence)
 
     if self._use_small_bfd:
       bfd_out_path = os.path.join(msa_output_dir, 'small_bfd_hits.sto')
